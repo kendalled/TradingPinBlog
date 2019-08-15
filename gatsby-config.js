c9111ab5cc19e43416a69756e5f88cf292676969
Kendall Jackson <kendallkj00@gmail.com>
2019-08-15 12:55:03 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: 'Trading Pin Blog: News and Discussions',
+    description: 'Trading Pins, Lapel Pins, and more - articles on a weekly basis.',
+    siteUrl: 'https://www.tradingpinblog.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: 'https://www.tradingpinblog.com',
       },
     },
     'gatsby-plugin-emotion',
